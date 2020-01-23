a6e528e2c77cb38ef36681b69fd2c458d4a702a5
exeynod <54329033+exeynod@users.noreply.github.com>
2020-01-23 12:09:50 +0300

    Create vk_bot.py

---
+++ Python-bot/vk_bot.py
@@ -0,0 +1,21 @@
+import vk_api
+from vk_api.utils import get_random_id
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+import requests
+
+vk_session = vk_api.VkApi(token="f068c796542cba0f4dbdd0f6e39ba656a489731d36cfdcbdf7cee30de822ae000aa9e1aa8293bc61d77c7")
+
+vk = vk_session.get_api()
+longpoll = VkBotLongPoll(vk_session, 191177272)
+
+for event in longpoll.listen():
+    if event.type == VkBotEventType.MESSAGE_NEW:
+        if event.obj.text != '':
+            # проверяем пришло сообщение от пользователя или нет
+            if event.from_user:
+                print(event.obj.get('message'))
+                vk.messages.send(
+                    user_id=event.obj.get('message').get('from_id'),
+                    random_id=get_random_id(),
+                    message=event.obj.get('message').get('text'),
+                )
